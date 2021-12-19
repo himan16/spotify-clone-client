47248b26da232a0731ae332a25a622fa997076f4
Himanshu Singhal <singhalhimanshu813@gmail.com>
2021-12-20 00:26:01 +0530

    initial commit

--- src/Login.js
+++ src/Login.js
@@ -2,7 +2,7 @@ import React from "react";
 import {Container} from "react-bootstrap";
 // const clientId = "9520eccbac5849a1bd5c98af590bfc0d";
 
-const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=9520eccbac5849a1bd5c98af590bfc0d&response_type=code&redirect_uri=https://stupefied-tesla-5734b1.netlify.app/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=9520eccbac5849a1bd5c98af590bfc0d&response_type=code&redirect_uri=https://spotifyclonewithlyrics.netlify.app/&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
 function Login(){
     return(
