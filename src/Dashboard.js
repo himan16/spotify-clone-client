214a99637c40b74ab6f53dee011ef2f5bea149c3
Himanshu Singhal <singhalhimanshu813@gmail.com>
2021-12-19 21:43:44 +0530

    initial commit

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -70,6 +70,7 @@ function Dashboard(props){
 
     return(
         <Container className="d-flex flex-column py-2" style={{height: "100vh"}}>
+            <div>{props.code}</div>
             <Form.Control
                 type='search'
                 placeholder="Search Songs/Artists"
